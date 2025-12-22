d002c254d9c8d8427bf162fe470926b2e7fb1dc2
owendominicantours-prog <249235156+proactivitis@users.noreply.github.com>
2025-12-22 01:07:38 -0400

    Remove checkout module

--- app/(public)/checkout/page.tsx
+++
@@ -1,9 +0,0 @@
-import CheckoutSummary from '@/components/checkout/CheckoutSummary';
-
-export default function CheckoutPage({
-  searchParams
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  return <CheckoutSummary params={searchParams} />;
-}
