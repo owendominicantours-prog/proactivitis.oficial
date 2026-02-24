58d414112cf906fec9c8cd35372cd3ea91b30aa5
owendominicantours-prog <249235156+proactivitis@users.noreply.github.com>
2026-02-24 11:45:50 -0400

    Fix 404 on /sosua/party-boat with permanent redirects

---
+++ app/(public)/fr/sosua/party-boat/page.tsx
@@ -0,0 +1,6 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function SosuaPartyBoatIndexRedirectFr() {
+  permanentRedirect("/fr/tours/party-boat-sosua");
+}
+
---
+++ app/(public)/sosua/party-boat/page.tsx
@@ -0,0 +1,6 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function SosuaPartyBoatIndexRedirect() {
+  permanentRedirect("/tours/party-boat-sosua");
+}
+
